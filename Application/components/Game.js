dd5333bbf373709b90fb2435744d822b5df08484
Toya-Onodera <gtrzexel@gmail.com>
2018-12-03 23:42:03 +0900

    とりあえず再帰を実装 TODO: ミニマックス法にしていく

--- Application/components/Game.js
+++ Application/components/Game.js
@@ -108,8 +108,8 @@ export default class App extends Component<Props> {
   // ○×の配列データを書き換える
   // setState ではインデックスを直接コピー出来ないのでこの方法を使用する
   _onPressTitTatToeButton (ind) {
-    const gameTitTatToeViewMaps_Copy = this.state.gameTitTatToeViewMaps.slice()
-    gameTitTatToeViewMaps_Copy[ind].value = ((this.state.playFirstPoint + this.state.drawFirstPoint) % 2 == 0) ? -10 : 10
+    let gameTitTatToeViewMaps_Copy = this.state.gameTitTatToeViewMaps.slice()
+    gameTitTatToeViewMaps_Copy[ind].value = ((this.state.playFirstPoint + this.state.drawFirstPoint) % 2 === 0) ? -10 : 10
     
     this.setState({
       gameTitTatToeViewMaps: gameTitTatToeViewMaps_Copy
@@ -143,11 +143,11 @@ export default class App extends Component<Props> {
   // 勝利判定を行うメソッド
   // ○ が勝利なら result の値が 1、 × が勝利なら 2、 試合が決まっていない or 引き分けなら 0
   // position の値が 0 -> 縦の左側、 1 -> 縦の中央、 2 -> 縦の右側、 3 -> 横の上側、 4 -> 横の中央、 5 -> 横の下側、 6 -> 斜めの左側が上、 7 -> 斜めの右側が上、 null -> 引き分け 
-  _judgmentGame () {
+  _judgmentGame (_gameTitTatToeViewMaps) {
     let i, j;
 
     // 確認用 (計算結果を代入) の変数
-    const gameTitTatToeViewMaps_Copy = this.state.gameTitTatToeViewMaps.slice()
+    const gameTitTatToeViewMaps_Copy = Object.assign({}, _gameTitTatToeViewMaps)
 
     // 縦
     for (i = 0; i < 3; i++) {
@@ -210,7 +210,7 @@ export default class App extends Component<Props> {
 
   // 勝利判定のあとの動作メソッド
   _winnerResult () {
-    const judgmentResult = this._judgmentGame()
+    const judgmentResult = this._judgmentGame(this.state.gameTitTatToeViewMaps)
     
     // 試合が決まったラインを表示するために state を変更
     this.setState({
@@ -222,11 +222,13 @@ export default class App extends Component<Props> {
   _goEmeny () {
     if (!(Object.keys(this.state.gameReason).length > 0 && this.state.gameReason.result > 0)) {
       /*** とりあえず適当な動作をする敵を作成 ***/
-      const priorityIndex = [4, 0, 2, 6, 7, 1, 3, 5, 7]
+      const turn = (this.state.playFirstPoint + this.state.drawFirstPoint)
+      const gameTitTatToeViewMaps_Copy = Object.assign({}, this.state.gameTitTatToeViewMaps)
+      //const priorityIndex = [4, 0, 8, 2, 6, 1, 7, 3, 5]
       
-      priorityIndex.some(e => {
+      /*priorityIndex.some(e => {
         if (this.state.gameTitTatToeViewMaps[e].value === 0) {
-          const gameTitTatToeViewMaps_Copy = this.state.gameTitTatToeViewMaps.slice()
+          let gameTitTatToeViewMaps_Copy = this.state.gameTitTatToeViewMaps.slice()
           gameTitTatToeViewMaps_Copy[e].value = ((this.state.playFirstPoint + this.state.drawFirstPoint) % 2 == 0) ? -10 : 10
           
           this.setState({
@@ -241,9 +243,57 @@ export default class App extends Component<Props> {
 
           return true
         }
-      })
+      })*/
 
       // TODO: ミニマックス法で実装する
+      //this._resultMinMax(turn, gameTitTatToeViewMaps_Copy, priorityIndex.concat(), 4)
+      this._resultMinMax(turn, gameTitTatToeViewMaps_Copy, 4)
+      //alert(this._resultMinMax(turn, gameTitTatToeViewMaps_Copy, priorityIndex.concat(), 4))
+      //minMax.resultMinMax()
+      /*const a = [1, 1, 1]
+      this._resultMiniMax(a.slice());*/
+      //alert(a)
+      // ((this.state.playFirstPoint + this.state.drawFirstPoint) % 2 == 0) ? -10 : 10
     }
+  }
+
+  // MiniMax 法で探索を行う関数
+  // turn -> 計算上でのターン
+  // gameVirtualStatus -> マス目の状況が格納されている変数
+  // availableIndex -> まだ値が格納されていないマス目
+  // putVirtualIndex -> その関数内で置いたマス番号
+  //_resultMinMax (turn, gameVirtualStatus, availableIndex, putedVirtualIndex) {
+    _resultMinMax (turn, gameVirtualStatus, putedVirtualIndex) {
+    const priorityIndex = [4, 0, 8, 2, 6, 1, 7, 3, 5]
+    
+    // 試合が決まっているか確認
+    const judgmentResult = this._judgmentGame(gameVirtualStatus)
+    
+    // 評価できる状態になったら
+    if (judgmentResult.result === 0) {
+      alert(gameVirtualStatus)
+      return
+    }
+
+    // 格納されていないマス目の数だけループし、探索を行う
+    priorityIndex.forEach((e, i) => {
+      // １マス１マス確認するため再帰を使用
+      if (gameVirtualStatus[e].value === 0) {
+        // 置いたマスを格納する
+        //alert(availableIndex)
+
+        // 置いたマスは除外する
+        //const putVirtualIndex = availableIndex.splice(i, 1)
+        
+        // 配列に足跡をつける
+        let gameVirtualStatus_Copy = JSON.parse(JSON.stringify(gameVirtualStatus))
+        gameVirtualStatus_Copy[e].value = (turn % 2 === 0) ? -10 : 10
+
+        // 計算できなくなるまで再帰
+        //this._resultMinMax(turn++, gameVirtualStatus_Copy, putVirtualIndex.concat(), e)
+        this._resultMinMax(turn++, gameVirtualStatus_Copy, e)
+        //
+      }
+    })
   }
 }
